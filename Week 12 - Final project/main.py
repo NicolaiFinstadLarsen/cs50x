924bd88acec12070eec8c33d8f0ff1dcdd74561d
Nicolai Finstad Larsen <nicolaifinstadlarsen@gmail.com>
2025-01-05 13:48:15 +0100

    Copied text in textbox
    Added code at top of script to delete table data and sett autoincrement sequence to 0 for each time user starts program. This to help with making textboxes.
    
    Added function and hotkey to open a new textbox for each copied text in database. Setting limit to 5 for now.

--- Week 12 - Final project/main.py
+++ Week 12 - Final project/main.py
@@ -10,6 +10,13 @@ try:
 except Exception as e:
     print(f"db=SQL error: {e}")
 
+try:
+    db.execute("DELETE FROM data")
+    db.execute("UPDATE sqlite_sequence SET seq = 0 WHERE name = 'data'")
+    print("table reset")
+except Exception as e:
+    print(f"Autoincrement reset error: {e}")
+
 # test = "some text to insert"
 
 # try:
@@ -26,8 +33,8 @@ root.title("Co.PY")
 label = tk.Label(root, text="Co.PY \n Copy & paste like a god", font=("Arial", 12))
 label.pack()
 
-textbox = tk.Text(root, height=2, width=30, padx=10)
-textbox.pack()
+# textbox = tk.Text(root, height=2, width=30, padx=10)
+# textbox.pack()
 
 def copy_to_clipboard():
     try:
@@ -41,27 +48,50 @@ def copy_to_clipboard():
         print("No text selected")
 
 
-def copy():
-    copied = textbox.get("1.0", "end-1c")
-    pyperclip.copy(copied)
+# def copy():
+#     copied = textbox.get("1.0", "end-1c")
+#     pyperclip.copy(copied)
 
 
-def paste():  
-    textbox.delete("1.0", "end")
-    pasted_text = pyperclip.paste()
-    textbox.insert("1.0", pasted_text)
-    print(f"Pasted from clipboard {pasted_text}")
+# def paste():  
+#     textbox.delete("1.0", "end")
+#     pasted_text = pyperclip.paste()
+#     textbox.insert("1.0", pasted_text)
+#     print(f"Pasted from clipboard {pasted_text}")
 
 
-copy_button = tk.Button(root, text="Copy", command=copy)
-copy_button.pack()
+def make_textbox():
+    try:
+        count = db.execute("SELECT COUNT(*) FROM data LIMIT 5")[0]["COUNT(*)"]
+        print(count)
+    except Exception as e:
+        print(f"Cant get db.row count: {e}")
+        return
 
-paste_button = tk.Button(root, text="Paste", command=paste)
-paste_button.pack()
+    for i in range(1, count+1): # Need to start at one to match SQL numbering.
+        try:
+            text = db.execute("SELECT copied_text FROM data WHERE id = ?", (i))[0]["copied_text"]
+        except Exception as e:
+            print(f"pulling text from db failed: {e}")
+            return
+
+        textbox = tk.Text(root, height=2, width=30, padx=10)
+        textbox.pack()
+        textbox.delete("1.0", "end")
+        textbox.insert("1.0", text)
+        i += 1
+
+
+# copy_button = tk.Button(root, text="Copy", command=copy)
+# copy_button.pack()
+
+# paste_button = tk.Button(root, text="Paste", command=paste)
+# paste_button.pack()
 
 
 keyboard.add_hotkey('ctrl+c', copy_to_clipboard)
-keyboard.add_hotkey('ctrl+v', paste)
+# keyboard.add_hotkey('ctrl+v', paste)
+keyboard.add_hotkey('ctrl+q', make_textbox)
 
 # Bind Ctrl+C and Ctrl+V to the copy_to_clipboard and paste functions
 #root.bind_all('<Control-c>', copy_to_clipboard)
